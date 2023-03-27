bc130fbf35e8a794efb015b5d0dafef9ff24ac2b
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-27 16:44:40 +0200

    agg

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ButtonComponent text="Cliccami!"/>
+        <ButtonComponent text="Oppure no"/>
         <ImageComponent src="https://picsum.photos/200" alt="Una bella lorem picsum"/>
+        <ImageComponent src="https://picsum.photos/100" alt="Una bella lorem picsum più piccola"/>
       </header>
     </div>
   );
