0ac7e60064fdbe1080188289675c7b9d8bed43c8
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-27 16:41:34 +0200

    che memoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ButtonComponent from './components/ButtonComponent';
+import ImageComponent from './components/ImageComponent';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ButtonComponent text="Cliccami!"/>
+        <ImageComponent src="https://picsum.photos/200" alt="Una bella lorem picsum"/>
       </header>
     </div>
   );
